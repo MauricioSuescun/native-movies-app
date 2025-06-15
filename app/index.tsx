2d74fc6e17699d75aa1a6b3304b6e810c8aaee9c
MauricioSuescun <110485721+MauricioSuescun@users.noreply.github.com>
2025-06-14 19:02:21 -0500

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+// import "../app/global.css";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-6xl font-bold text-primary">Puto.</Text>
+    </View>
+  );
+}
