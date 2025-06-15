056418edebce04978935e56a7643c6ae4a1af009
MauricioSuescun <110485721+MauricioSuescun@users.noreply.github.com>
2025-06-15 15:12:42 -0500

    api rendering

---
+++ services/useFecth.ts
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+const useFetch = <T>(fetchFunction: () => Promise<T>, autoFetch = true) => {
+  const [data, setData] = useState<T | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [loading, setLoading] = useState(autoFetch);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+      const result = await fetchFunction();
+      setData(result);
+    } catch (err) {
+      setError(
+        err instanceof Error ? err : new Error("Unknown error occurred")
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const reset = () => {
+    setData(null);
+    setError(null);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (autoFetch) {
+      fetchData();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return { data, error, loading, refetch: fetchData, reset };
+};
+
+export default useFetch;
